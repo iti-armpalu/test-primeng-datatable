b8ed69e857a9fc5f61faef05b411e882e7fce8ab
Iti <iti.armpalu@gmail.com>
2022-09-15 13:18:52 +0400

    Design fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,7 +60,7 @@ export class AppComponent {
       id: 1,
       title: "Success",
       hex: ColorBaseSuccess,
-      designToken: "---color-base-success"
+      designToken: "--color-base-success"
     },
     {
       id: 2,
@@ -87,85 +87,91 @@ export class AppComponent {
       id: 1,
       title: "Chart 1",
       hex: ColorChart1,
-      designToken: "---color-chart-1"
+      designToken: "--color-chart-1"
     },
     {
       id: 2,
       title: "Chart 2",
       hex: ColorChart2,
-      designToken: "---color-chart-2"
+      designToken: "--color-chart-2"
     },
     {
       id: 3,
       title: "Chart 3",
       hex: ColorChart3,
-      designToken: "---color-chart-3"
+      designToken: "--color-chart-3"
     },
     {
       id: 4,
       title: "Chart 4",
       hex: ColorChart4,
-      designToken: "---color-chart-4"
+      designToken: "--color-chart-4"
     },
     {
       id: 5,
       title: "Chart 5",
       hex: ColorChart5,
-      designToken: "---color-chart-5"
+      designToken: "--color-chart-5"
     },
     {
       id: 6,
       title: "Chart 6",
       hex: ColorChart6,
-      designToken: "---color-chart-6"
+      designToken: "--color-chart-6"
+    },
+    {
+      id: 7,
+      title: "Chart 7",
+      hex: ColorChart7,
+      designToken: "--color-chart-7"
     },
     {
       id: 8,
       title: "Chart 8",
       hex: ColorChart8,
-      designToken: "---color-chart-8"
+      designToken: "--color-chart-8"
     },
     {
       id: 9,
       title: "Chart 9",
       hex: ColorChart9,
-      designToken: "---color-chart-9"
+      designToken: "--color-chart-9"
     },
     {
       id: 10,
       title: "Chart 10",
       hex: ColorChart10,
-      designToken: "---color-chart-10"
+      designToken: "--color-chart-10"
     },
     {
       id: 11,
       title: "Chart 11",
       hex: ColorChart11,
-      designToken: "---color-chart-11"
+      designToken: "--color-chart-11"
     },
     {
       id: 12,
       title: "Chart 12",
       hex: ColorChart12,
-      designToken: "---color-chart-12"
+      designToken: "--color-chart-12"
     },
     {
       id: 13,
       title: "Male",
       hex: ColorGenderMale,
-      designToken: "---color-gender-male"
+      designToken: "--color-gender-male"
     },
     {
       id: 14,
       title: "Female",
-      hex: ColorGenderMale,
-      designToken: "---color-gender-female"
+      hex: ColorGenderFemale,
+      designToken: "--color-gender-female"
     },
     {
       id: 15,
       title: "Non-biary",
       hex: ColorGenderNonBinary,
-      designToken: "---color-gender-non-binary"
+      designToken: "--color-gender-non-binary"
     }
   ]
 
